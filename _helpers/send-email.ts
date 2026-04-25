bd0463a2a934ac4eac5cf5a54f6f63f5c3496fce
natsukidxd <natsukid123@gmail.com>
2026-04-25 20:05:41 +0800

    fix: correct casing in SMTP options for email transport configuration

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 import config from '../config.json';
 export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
-const transporter = nodemailer.createTransport(config.smtpOptions);
+    const transporter = nodemailer.createTransport(config.smtpoptions);
 await transporter.sendMail({ from, to, subject, html });
 }
